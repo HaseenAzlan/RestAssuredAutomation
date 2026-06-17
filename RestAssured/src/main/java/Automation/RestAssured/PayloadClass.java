b62bdd0fe2ea85a4147b7e4cbac5fb43420f796e
Haseena Shaik <skhaseena.0201@gmail.com>
2026-06-17 18:32:08 +0530

    Dynamic Json

--- RestAssured/src/main/java/Automation/RestAssured/PayloadClass.java
+++ RestAssured/src/main/java/Automation/RestAssured/PayloadClass.java
@@ -78,7 +78,16 @@ public class PayloadClass {
 				+ "}";
 		return complexJson;
 	}
-	
+	public static String addbookPayload(String isbn, String aisle ) {
+		String payload= "{\r\n"
+				+ "\"name\":\"Learn Appium Automation with Java\",\r\n"
+				+ "\"isbn\":\""+isbn+"\",\r\n"
+				+ "\"aisle\":\""+aisle+"\",\r\n"
+				+ "\"author\":\"John foe\"\r\n"
+				+ "}\r\n"
+				+ "";
+		return payload;
+	}
 	
 
 }
